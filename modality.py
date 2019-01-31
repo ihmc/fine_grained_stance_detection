a394f1ba0453a14f1ac70253e1e1c3a32a4804a3
Brodie Mather <bmather@ihmc.us>
2019-01-31 15:20:48 -0600

    Added checks for bigrams and trigrams

--- modality.py
+++ modality.py
@@ -16,8 +16,15 @@ with open('./20190130-Dorr-Modality-Baseline-Lexicon.csv') as modalityCSV:
     csvReader = csv.reader(modalityCSV)
     for word, pos, otherWord, owPOS, nextWord, nwPOS, modality, example in csvReader:
         for pos in pos.split("|"):
-            modalityLookup[(word, pos)] = modality
-            modalityLookup[((word, pos), (otherWord, owPOS))] = modality
+            if otherWord:
+                if nextWord:
+                    modalityLookup[((word, pos), (otherWord, owPOS), (nextWord, nwPOS))] = modality
+                else:
+                    modalityLookup[((word, pos), (otherWord, owPOS))] = modality
+            else:
+                modalityLookup[(word, pos)] = modality
+            
+            
             # TODO: trigram modality
 
 app = Flask(__name__)
@@ -37,8 +44,9 @@ def handleEmail():
         pos_tags = nltk.pos_tag(words)
         unigrams = [(morphRoot(tup[0].lower()), tup[1]) for tup in pos_tags]
         bigrams = list(zip(unigrams, unigrams[1:-1]))
+        trigrams = list(zip(unigrams, unigrams[1:-1], unigrams[1:-2]))
 
-        modal_words = (unigrams & modalityLookup.keys()) | (bigrams & modalityLookup.keys())
+        modal_words = (unigrams & modalityLookup.keys()) | (bigrams & modalityLookup.keys()) | (trigrams &  modalityLookup.keys())
         modals = [(modal, modalityLookup[modal]) for modal in modal_words]
 
         sentence_modalities.append({"sentence": sentence, "modals": list(modals), "pos": list(pos_tags)})
