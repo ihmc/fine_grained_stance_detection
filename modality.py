9b4fa103343bef9f264825ecf4b4e43c375bc4d6
Brodie Mather <bmather@ihmc.us>
2019-02-05 12:49:56 -0600

    Added local reading and updated lexicon

--- modality.py
+++ modality.py
@@ -8,23 +8,31 @@ from nltk.corpus import wordnet as wn
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 import csv
+import os
 
-modalityLookup = {}
+modality_lookup = {}
 sentence_modalities = []
 
-with open('./20190130-Dorr-Modality-Baseline-Lexicon.csv') as modalityCSV:
+# When reading in files locally these directorys must be inside the project directory(i.e. mood and modality)
+# They can be named whatever you would like just make sure they exists
+# All files meant to be read in should be in the text directory.
+# The output directory will be filled with the modality results of processing the input files
+input_directory = '/text/'
+output_directory = '/output/'
+
+with open('./20190204-Dorr-Modality-Lexicon.v1.csv') as modalityCSV:
     csvReader = csv.reader(modalityCSV)
-    for word, pos, otherWord, owPOS, nextWord, nwPOS, modality, example in csvReader:
+    for word, pos, otherWord, owPOS, nextWord, nwPOS, modality in csvReader:
         for pos in pos.split("|"):
             if otherWord:
                 for owPos in owPOS.split("|"):
                     if nextWord:
                         for nwPos in nwPOS.split("|"):
-                            modalityLookup[((word, pos), (otherWord, owPos), (nextWord, nwPos))] = modality
+                            modality_lookup[((word, pos), (otherWord, owPos), (nextWord, nwPos))] = modality
                     else:
-                        modalityLookup[((word, pos), (otherWord, owPos))] = modality
+                        modality_lookup[((word, pos), (otherWord, owPos))] = modality
             else:
-                modalityLookup[(word, pos)] = modality
+                modality_lookup[(word, pos)] = modality
 
 app = Flask(__name__)
 
@@ -36,8 +44,22 @@ def hello():
 def handleEmail():
     payload = request.get_json();
 
-    sentences = nltk.sent_tokenize(payload['text'])
+    
+    sentence_modalities = getModality(payload['text'])
+
+    return json.dumps(sentence_modalities)
+
+@app.route("/local", methods = ['POST', 'GET'])
+def handleLocalRead():
+    readLocalFiles()
+
+    return "Finished. What's finsihed? Who knows, just finsihed"
+
+
+def getModality(text):
+    sentences = nltk.sent_tokenize(text)
     sentence_modalities = []
+
     for sentence in sentences:
         words = nltk.word_tokenize(sentence)
         pos_tags = nltk.pos_tag(words)
@@ -45,13 +67,26 @@ def handleEmail():
         bigrams = list(zip(unigrams, unigrams[1:-1]))
         trigrams = list(zip(unigrams, unigrams[1:-1], unigrams[2:-1]))
 
-        modal_words = (unigrams & modalityLookup.keys()) | (bigrams & modalityLookup.keys()) | (trigrams &  modalityLookup.keys())
-        modals = [(modal, modalityLookup[modal]) for modal in modal_words]
+        modal_words = (unigrams & modality_lookup.keys()) | (bigrams & modality_lookup.keys()) | (trigrams &  modality_lookup.keys())
+        modals = [(modal, modality_lookup[modal]) for modal in modal_words]
 
         sentence_modalities.append({"sentence": sentence, "modals": list(modals), "pos": list(pos_tags)})
 
+    return sentence_modalities
 
-    return json.dumps(sentence_modalities)
+def readLocalFiles():
+    path = os.path.abspath(os.path.dirname(__file__))
+    inputPath  = path + input_directory
+    outputPath = path + output_directory
+
+    for filename in os.listdir(inputPath):
+        with open(inputPath + filename, 'r') as input_file:
+            text = input_file.read()
+            with open(outputPath + 'ouput' + filename + '.json', 'w') as output_file:
+                json_modality = getModality(text)
+                output_file.write(json.dumps(json_modality, indent=4, sort_keys=False))
+
+    return 
 
 def morphRoot(word):
     wlem = WordNetLemmatizer()
