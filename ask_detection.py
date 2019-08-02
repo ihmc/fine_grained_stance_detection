657adb03f4217d4507db1b7953e8c2898a1515f4
Brodie Mather <bmather@ihmc.us>
2019-08-02 17:02:21 -0500

    Removed line profiler import as it was only need to test time use of code

--- ask_detection.py
+++ ask_detection.py
@@ -1,7 +1,6 @@
 from allennlp.predictors.predictor import Predictor
 predictor = Predictor.from_path("https://s3-us-west-2.amazonaws.com/allennlp/models/srl-model-2018.05.25.tar.gz")
 
-import line_profiler
 import unicodedata
 import json
 import nltk
